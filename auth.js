a1f122d75311b2dec75ed8b30a40e0d6f35f13c1
earlgreyrosetea <adriana.m.cruz02@gmail.com>
2025-03-07 14:34:04 -0800

    Update auth.js for channel

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 const auth = (function () {
 	// Authentication and channels - required
-	const channel = ""; // your channel
-	const username = ""; // bot account or your channel
+	const channel = "earlgreyrosetea"; // your channel
+	const username = "earlgreyrosetea"; // bot account or your channel
 
 	// You may or may not include the "oauth:" portion of your token
-	const oauth = "";
+	const oauth = "dx0udpr0mqcvtaahwweyxjycsxstxx";
 
 	return {
 		channel,
